e222ce7311e7c44f5bd868f715a8754785dd1637
Luiz Francisco <luizfk.gmail.com>
2024-06-14 16:24:25 -0300

    tarefa modulo 26

---
+++ src/terefa_modulo26.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplicacao = (num1, num2) => num1 * num2;
+const meuNome = (nome) => `Olá ${nome} `;
+console.log(multiplicacao(10, 30));
+console.log(meuNome('Luiz'));
---
+++ src/terefa_modulo26.ts
@@ -0,0 +1,6 @@
+const multiplicacao = (num1: number, num2: number): number => num1 * num2
+
+const meuNome = (nome: string): string => `Olá ${nome} `;
+
+console.log(multiplicacao(10,30))
+console.log(meuNome('Luiz'))
